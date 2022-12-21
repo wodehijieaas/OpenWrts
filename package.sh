af7415c25bc4d0965192c5559be0d771e38e8dc1
wodehijieaas <39908171+wodehijieaas@users.noreply.github.com>
2022-12-21 15:16:04 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -2,7 +2,7 @@
 git clone  https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
 git clone  https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
 git clone  https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
-git clone  https://github.com/kkkidding/openwrt-qbittorrent/tree/master/luci-app-qbittorrent package/otherapp/luci-app-qbittorrent
+git clone  https://github.com/kkkidding/openwrt-qbittorrent.git package/otherapp/luci-app-qbittorrent
 # vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/otherapp/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/otherapp/luci-app-vssr
